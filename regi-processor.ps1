b12b45bae6863e068f4a8167e6a51dd647ed3259
The Watcher <34960106+xd20111@users.noreply.github.com>
2024-03-17 17:42:14 +0530

    regi-processor.ps1 v3

--- regi-processor.ps1
+++ regi-processor.ps1
@@ -1,19 +1,18 @@
-
 $baseRegistryPath = "HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\"
 
 $existingFolders = Get-ChildItem -Path $baseRegistryPath | Where-Object { $_.PSChildName -match '^\d+$' } | ForEach-Object { $_.PSChildName }
 
+$newProcessorName = Read-Host "Enter the new processor name"
+
 foreach ($folder in $existingFolders) {
     $registryPath = "$baseRegistryPath$folder"
-    
+
     $currentValue = Get-ItemProperty -Path $registryPath | Select-Object -ExpandProperty ProcessorNameString
 
-
-    $newValueData = "Update V2"
-    if ($currentValue -ne $newValueData) {
-        Set-ItemProperty -Path $registryPath -Name ProcessorNameString -Value $newValueData
-        Write-Host "Value updated for ProcessorNameString $folder"
+    if ($currentValue -ne $newProcessorName) {
+        Set-ItemProperty -Path $registryPath -Name ProcessorNameString -Value $newProcessorName
+        Write-Host "Value updated for  Processor Name $folder"
     } else {
-        Write-Host "Value already up-to-date for ProcessorNameString $folder"
+        Write-Host "Value already up-to-date for Processor Name $folder"
     }
 }
