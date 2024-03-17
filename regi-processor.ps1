f9fa274ead95a00d9cba3cf98c5bf4c5568e8d46
The Watcher <34960106+xd20111@users.noreply.github.com>
2024-03-17 17:40:07 +0530

    Update regi-processor.ps1

--- regi-processor.ps1
+++ regi-processor.ps1
@@ -1,13 +1,13 @@
-# Define the base registry path
+
 $baseRegistryPath = "HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\"
 
-# Define the range of folders (0 to 9)
+
 $foldersRange = 0..9
 
 foreach ($folder in $foldersRange) {
     $registryPath = "$baseRegistryPath$folder"
     
-    # Check if the registry key exists
+
     if (Test-Path $registryPath) {
         # Get the current value of ProcessorNameString
         $currentValue = Get-ItemProperty -Path $registryPath | Select-Object -ExpandProperty ProcessorNameString
