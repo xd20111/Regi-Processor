861d15f32ba81fc1a0e3582f67ac6a5b3d5dc5e0
The Watcher <34960106+xd20111@users.noreply.github.com>
2024-03-17 17:50:59 +0530

    regi-processor.ps1 v3

--- regi-processor.ps1
+++ regi-processor.ps1
@@ -1,14 +1,19 @@
+# Define the base registry path
 $baseRegistryPath = "HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\"
 
+# Get the list of existing folders under the base registry path
 $existingFolders = Get-ChildItem -Path $baseRegistryPath | Where-Object { $_.PSChildName -match '^\d+$' } | ForEach-Object { $_.PSChildName }
 
+# Prompt the user to enter the new processor name
 $newProcessorName = Read-Host "Enter the new processor name"
 
 foreach ($folder in $existingFolders) {
     $registryPath = "$baseRegistryPath$folder"
-
+    
+    # Get the current value of ProcessorNameString
     $currentValue = Get-ItemProperty -Path $registryPath | Select-Object -ExpandProperty ProcessorNameString
 
+    # Modify the value if needed
     if ($currentValue -ne $newProcessorName) {
         Set-ItemProperty -Path $registryPath -Name ProcessorNameString -Value $newProcessorName
         Write-Host "Value updated for  Processor Name $folder"
